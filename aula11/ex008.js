0f85c87c7797da7333e169be400d242050fd7d64
jonatas <jhownsbidu@outlook.com>
2025-05-06 11:32:22 -0300

    Criando alguns site em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 41
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+
+if(vel > 40){
+    console.log(`Você passou a velocidade permitida, MULTADO`)
+}
+
+
+console.log(`Dirija sempre com cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var país = 'França'
+
+console.log(`Vivendo em ${país} da melhor forma possível`)
+ 
+if ( país == 'Brasil') {
+    console.log(` Você é brasileiro`)
+
+} else {
+    console.log('Você é estrageiro')
+}
